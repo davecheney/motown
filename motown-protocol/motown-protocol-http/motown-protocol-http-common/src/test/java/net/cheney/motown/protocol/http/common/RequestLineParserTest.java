362c2b1463ecaae8f5c2ef5cffac143db8591a38
Dave Cheney <dave@cheney.net>
2010-01-03 21:45:15 +1100

    More tests

--- motown-protocol/motown-protocol-http/motown-protocol-http-common/src/test/java/net/cheney/motown/protocol/http/common/RequestLineParserTest.java
+++ motown-protocol/motown-protocol-http/motown-protocol-http-common/src/test/java/net/cheney/motown/protocol/http/common/RequestLineParserTest.java
@@ -27,5 +27,23 @@ public class RequestLineParserTest {
 		assertEquals(r.uri(), URI.create("/foo"));
 		assertEquals(r.version(), Version.HTTP_1_0);
 	}
+	
+	@Test
+	public void testGETRequestWithTrailingData() {
+		RequestLine r = parser.parse("GET /foo HTTP/1.0\r\nHost: localhost\r\n\r\n");
+		assertEquals(r.method(), Method.GET);
+		assertEquals(r.uri(), URI.create("/foo"));
+		assertEquals(r.version(), Version.HTTP_1_0);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testUnknowMethod() {
+		parser.parse("BRICK /foo HTTP/1.0\r\n");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testUnknownHttpVersion() {
+		parser.parse("POST /foo HTTP/2.1\r\n");
+	}
 
 }
