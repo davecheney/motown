16038dfea1be3fbd6dc65191297a26a9c1f39599
Dave Cheney <dave@cheney.net>
2010-01-07 16:08:36 +1100

    Cleanup whack dependancys

--- motown-api/src/test/java/net/cheney/motown/api/HeaderAccessorTestCase.java
+++ motown-api/src/test/java/net/cheney/motown/api/HeaderAccessorTestCase.java
@@ -1,15 +1,11 @@
 package net.cheney.motown.api;
 
-import static net.cheney.motown.api.Header.HOST;
 import static net.cheney.motown.api.Header.VIA;
 import static org.junit.Assert.assertEquals;
 
 import java.net.URISyntaxException;
-import java.nio.ByteBuffer;
 import java.util.Iterator;
 import java.util.List;
-
-import net.cheney.motown.protocol.http.common.RequestParser;
 
 import org.junit.Test;
 
@@ -17,14 +13,6 @@ import com.google.common.collect.Lists;
 
 public class HeaderAccessorTestCase {
 
-	@Test
-	public void testRequestHeaderAccessor() {
-		String s = "PROPFIND /Software%20Update%20Enabler.app HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: WebDAVFS/1.8 (01808000) Darwin/10.2.0 (i386)\r\nAccept: */*\r\nContent-Type: text/xml\r\nDepth: 0\r\nContent-Length: 161\r\nConnection: keep-alive\r\n\r\n";
-		Request request = new RequestParser().parse(ByteBuffer.wrap(s.getBytes()));
-		
-		assertEquals(request.header(HOST).getOnlyElement(), "localhost:8080");
-	}
-	
 	@Test 
 	public void testHeaderSet() throws URISyntaxException {
 		Request r = new Request(Method.GET, "/", Version.HTTP_1_1);
