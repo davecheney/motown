1aa839250e0e8edfab8832c2e8eceeba99d86c3b
Dave Cheney <dave@cheney.net>
2010-03-26 22:43:42 +1100

    Removed imports and methods from Env
    The Env should know as little as possible about the thing that created it. Ideally yhis will be Env.fromRequest()

--- motown-server/motown-server-api/src/main/java/net/cheney/motown/server/api/Environment.java
+++ motown-server/motown-server-api/src/main/java/net/cheney/motown/server/api/Environment.java
@@ -1,12 +1,9 @@
 package net.cheney.motown.server.api;
 
-import static net.cheney.motown.common.api.Header.DEPTH;
-
 import java.net.URI;
 
 import com.google.common.collect.Multimap;
 
-import net.cheney.motown.common.api.Depth;
 import net.cheney.motown.common.api.Header;
 import net.cheney.motown.common.api.Request;
 import net.cheney.motown.common.api.Version;
@@ -34,11 +31,6 @@ public abstract class Environment {
 			}
 			
 			@Override
-			public Multimap<Header, String> headers() {
-				return req.headers();
-			}
-			
-			@Override
 			public HeaderAccessor<Request> header(Header header) {
 				return req.header(header);
 			}
@@ -50,8 +42,6 @@ public abstract class Environment {
 	public abstract Version version();
 
 	public abstract URI uri();
-
-	public abstract Multimap<Header, String> headers();
 
 	public abstract HeaderAccessor<Request> header(Header header);
 	
